0717e1c850d1bcc0029aac3222e234ef41eea606
paalon <paalon1936@gmail.com>
2016-09-21 19:58:56 +0900

    add Window.js an WindowOption.js

---
+++ scene/Window.js
@@ -0,0 +1,25 @@
+// Window.js
+// Copyright 2016 Paalon
+//
+// class Window
+//
+
+let PIXI = require('pixi.js/bin/pixi.js');
+
+module.exports = class Window extends PIXI.Container {
+  constructor(options) {
+    super();
+    this.keyboard = options.keyboard; // キーボード操作
+    this.content = options.content;
+    this.style = options.style;
+  }
+  init() {
+
+  }
+  stop() {
+  }
+  pause() {
+  }
+  play() {
+  }
+};
