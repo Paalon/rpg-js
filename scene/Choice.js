4a58902f76df16bf3ce9e3b82aa4e8ed889915b9
paalon <paalon1936@gmail.com>
2016-09-23 00:01:37 +0900

    add done func

--- scene/Choice.js
+++ scene/Choice.js
@@ -8,7 +8,11 @@ let PIXI = require('pixi.js/bin/pixi.js');
 
 module.exports = class Choice extends PIXI.Text {
   constructor(text, style, done) {
+    if (done == undefined) throw new Error('完了したときの動作は書いとけよ。');
     super(text, style);
-    this.done = done;
+    this._done = done;
+  }
+  done(option) {
+    this._done(option);
   }
 };
